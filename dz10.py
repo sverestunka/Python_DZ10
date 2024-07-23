8482f0f673875b48bc152e9b18baaeb441a2ebbf
Svetlana_Verestun <sverestunka@mail.ru>
2024-07-23 17:17:20 +0300

    ДЗ урок 10

---
+++ dz10.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
+
+for value in set(lst):
+    data[value] = (data['whoAmI'] == value).astype(int)
+    
+data.drop(columns=['whoAmI'], inplace=True)
+
+data.head()
