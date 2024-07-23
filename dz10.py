ec9c1c0d4e2041676249d930d44d606f70943c4e
Svetlana_Verestun <sverestunka@mail.ru>
2024-07-23 17:38:05 +0300

    change2

--- dz10.py
+++ dz10.py
@@ -4,10 +4,9 @@ lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
-data.head()
 
 for value in set(lst):
     data[value] = (data['whoAmI'] == value).astype(int)
-    
+
 data.drop(columns=['whoAmI'], inplace=True)
 data.head()
